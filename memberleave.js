9387ef7f11fc86037d31d0caf0ae069d7d5a37b1
Aarian Ahsan <44757353+Aarian-A@users.noreply.github.com>
2022-01-19 20:23:06 -0600

    Delete memberleave.js

--- memberleave.js
+++
@@ -1,4 +0,0 @@
-client.on('guildMemberRemove', async(member) => { 
-    const Channel = member.guild.channels.cache.get('759670370558541845')
-    Channel.send('**${member.displayName}** has left ${member.guild.name}')
-})
