265efddcab90c58efd43ed1837218aecc710146a
Aarian Ahsan <44757353+Aarian-A@users.noreply.github.com>
2022-01-19 18:07:50 -0600

    Updating bot.js

--- Discordbot/bot.js
+++ Discordbot/bot.js
@@ -1,16 +1,44 @@
 const { Client, Intents, Message, MessageEmbed } = require('discord.js');
 
-const client = new Client({intents : [Intents.FLAGS.GUILD_MESSAGES , Intents.FLAGS.GUILDS]});
+const client = new Client({intents : [Intents.FLAGS.GUILD_MESSAGES , Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS , Intents.FLAGS.GUILDS , Intents.FLAGS.DIRECT_MESSAGE_REACTIONS , Intents.FLAGS.GUILD_MESSAGE_REACTIONS]});
 
-const prefix = "&";
+const prefix = "-";
 
 client.once('ready', () => {
     console.log('Sado is online!');
 });
 
+
+client.on('guildMemberAdd', (member) => {
+  console.log(member)
+  const welcomeEmbed = new Discord.MessageEmbed()
+
+  welcomeEmbed.setColor('#000000')
+  welcomeEmbed.setTitle('**' + member.user.username + '** has joined us other **' + member.guild.memberCount + '** unswallowed members')
+  welcomeEmbed.setImage('https://i.imgflip.com/2fbyuw.jpg')
+
+  member.guild.channels.cache.find(i => i.name === 'announcements').send(welcomeEmbed)
+})
+
+
+/*
+//added
+client.on('guildMemberAdd', async(member) => { 
+  const Channel = member.guild.channels.cache.get('759670370558541845')
+  Channel.send(`**${member.displayName}** has joined ${member.guild.name}`)
+})
+
+//left
+client.on('guildMemberRemove', async(member) => { 
+  const Channel = member.guild.channels.cache.get('759670370558541845')
+  Channel.send(`**${member.displayName}** has left ${member.guild.name}`)
+})
+*/
+
+
 client.on('message', message => {
   switch(message.content.toUpperCase()) {
-      case '?RESET':
+      case '-RESET':
           resetBot(message.channel);
           break;
 
@@ -25,23 +53,46 @@ client.on('message', message => {
     const command = args.shift();
   
     if(command === 'sado'){
-      message.channel.send('chad');
+      message.channel.send('hermano momento');
     }
+
+    if(command === 'waah'){
+      message.channel.send(':waaah:');
+    }
+
   });
 
   function resetBot(channel) {
     // send channel a message that you're resetting bot [optional]
-    channel.send('Resetting...')
+    channel.send('Reset!')
     .then(msg => client.destroy())
     .then(() => client.login('token'));
-    channel.send('Reset!')
-  }
+}
 /*
-client.on('guildMemberRemove', async(member) => { 
-    const Channel = member.guild.channels.cache.get('759670370558541845')
-    Channel.send('**${member.displayName}** has left ${member.guild.name}')
+
+//left
+client.on("guildMemberRemove", member => {
+  const welcome = member.guild.channels.cache.find(channel => channel.name === 'announcements')
+  welcome.send (`${member} fell down a hole`)
 })
 
+//left
+client.on('guildMemberRemove',(member) => {
+  client.channels.find('log_channel').send('**${member.username}** has just left server.. Bye Bye')
+})
+
+//added
+client.on("guildMemberAdd", member => {
+  const welcome = member.guild.channels.cache.find(channel => channel.name === 'announcements')
+  welcome.send (`${member} is among us`)
+})
+
+//added
+client.on('guildMemberAdd', member => {
+  member.guild.channels.get('759670370558541845').send("Welcome"); 
+});
+
 */
-client.login('token');
+
+client.login('OTMzMjkzNTg4NDQ2NTkzMDM1.YefbYw.VHvZRfxXUpZVgVhet7THS15etsw');
 
